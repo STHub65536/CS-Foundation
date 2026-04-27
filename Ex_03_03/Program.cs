73358615d5dd935175ab36600ff934c2e440793c
STHub65536 <2026opnewemp006@fullness.email>
2026-04-27 12:33:29 +0900

    Ex_03_03解答修正

--- Ex_03_03/Program.cs
+++ Ex_03_03/Program.cs
@@ -4,6 +4,18 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Item item = new Item();
+
+        Console.Write("商品番号を入力してください->");
+        item.itemNo = int.Parse(Console.ReadLine());
+        Console.Write("商品名を入力してください->");
+        item.itemName = Console.ReadLine();
+        Console.Write("単価を入力してください->");
+        item.price = int.Parse(Console.ReadLine());
+        Console.Write("新しい単価を入力してください->");
+        item.ChangePrice(int.Parse(Console.ReadLine()));
+
+        Console.WriteLine("結果");
+        item.Print();
     }
 }
