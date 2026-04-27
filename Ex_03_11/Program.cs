96012fea70dcae71c97f45b8f1a664f3f8787ee1
STHub65536 <2026opnewemp006@fullness.email>
2026-04-27 16:52:49 +0900

    finished Ex_03_11

---
+++ Ex_03_11/Program.cs
@@ -0,0 +1,18 @@
+﻿using Ex_03_11.Modules;
+
+namespace Ex_03_11;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Mouse mouse = new Mouse();
+        FlashMemory flashMemory = new FlashMemory();
+        Display display = new Display();
+
+        mouse.SendData();
+        flashMemory.SendData();
+        flashMemory.ReceiveData();
+        display.ReceiveData();
+    }
+}
