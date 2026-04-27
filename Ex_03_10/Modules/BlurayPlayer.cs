03907ffe613e93f39c45f4f492ea21356fd6ee6b
STHub65536 <2026opnewemp006@fullness.email>
2026-04-27 16:20:22 +0900

    finished Ex_03_10

---
+++ Ex_03_10/Modules/BlurayPlayer.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_10.Modules
+{
+    public class BlurayPlayer : Player
+    {
+        public BlurayPlayer(string media):base(media){}
+
+        public override void Play()
+        {
+            Console.WriteLine("Blu-ray:"+base.Media+"を再生しています");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Blu-ray:"+base.Media+"の再生を停止しました");
+        }
+    }
+}
---
+++ Ex_03_10/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ex_03_10.Modules;
+
+namespace Ex_03_10;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("DVDのタイトルを入力してください->");
+        DVDPlayer dvdPlayer = new DVDPlayer(Console.ReadLine());
+        dvdPlayer.Play();
+        dvdPlayer.Stop();
+
+        Console.Write("Blu-rayのタイトルを入力してください->");
+        BlurayPlayer blurayPlayer = new BlurayPlayer(Console.ReadLine());
+        blurayPlayer.Play();
+        blurayPlayer.Stop();
+    }
+}
