231d62b2fb117cf4419e86a9d63944f98d5ad02e
STHub65536 <2026opnewemp006@fullness.email>
2026-04-27 14:11:29 +0900

    Ex_03_07解答

---
+++ Ex_03_07/Models/Item.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_07.Models
+{
+    /// <summary>
+    /// 商品情報を表すクラス
+    /// </summary>
+    public class Item
+    {
+        private int itemNo;
+        private string itemName;
+        private int price;
+
+        public int ItemNo{ get; set; }
+        public string ItemName{ get; set; }
+        public int Price{ get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("結果");
+            Console.WriteLine("商品番号="+this.ItemNo);
+            Console.WriteLine("商品名="+this.ItemName);
+            Console.WriteLine("単価="+this.Price);
+        }
+    }
+}
---
+++ Ex_03_07/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ex_03_07.Models;
+
+namespace Ex_03_07;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Item item = new Item();
+
+        Console.Write("商品番号を入力してください->");
+        item.ItemNo = int.Parse(Console.ReadLine());
+        Console.Write("商品名を入力してください->");
+        item.ItemName = Console.ReadLine();
+        Console.Write("単価を入力してください->");
+        item.Price = int.Parse(Console.ReadLine());
+
+        item.Print();
+    }
+}
