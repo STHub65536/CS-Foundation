410204b4fd661a90436b351527af17661a2b6d54
STHub65536 <2026opnewemp006@fullness.email>
2026-04-27 14:39:03 +0900

    Ex_03_08解答

---
+++ Ex_03_08/Modules/Account.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_08.Modules
+{
+    public class Account
+    {
+        private string? accountNo;
+        private string? accounttName;
+        private int balance;
+
+        public string AccountNo{ get; set; }
+        public string AccountName{ get; set; }
+        public int Balance{ get; set; }
+    }
+}
---
+++ Ex_03_08/Modules/NetAccount.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_08.Modules
+{
+    public class NetAccount : Account
+    {
+        private int point;
+
+        public int Point{ get; set; }
+    }
+}
