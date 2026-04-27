54e89fc3fc0bd82298955b50042a9e5baf9327c5
STHub65536 <2026opnewemp006@fullness.email>
2026-04-27 12:32:32 +0900

    Ex_03_03解答

---
+++ Ex_03_03/Item.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_03
+{
+    /// <summary>
+    /// 商品情報を表すクラス
+    /// </summary>
+    public class Item
+    {
+        public int itemNo;
+        public string itemName;
+        public int price;
+
+        public void Print()
+        {
+            Console.WriteLine("商品番号="+this.itemNo);
+            Console.WriteLine("商品名="+this.itemName);
+            Console.WriteLine("単価="+this.price);
+        }
+
+        public void ChangePrice(int price)
+        {
+            this.price = price;
+        }
+    }
+}
