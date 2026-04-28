ef04abdb68490c81985c5957b9638a4d3eef3de9
STHub65536 <2026opnewemp006@fullness.email>
2026-04-28 12:21:55 +0900

    finished Ex_04_02

---
+++ Ex_04_02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_04_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("年を入力してください->");
+        int month = int.Parse(Console.ReadLine());
+
+        if (DateTime.IsLeapYear(month))
+        {
+            Console.WriteLine(month+"年は閏年です。");
+        }
+        else
+        {
+            Console.WriteLine(month+"年は閏年ではありません。");
+        }
+
+    }
+}
