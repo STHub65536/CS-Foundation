8c845b6ac6fddfa1f00be25c88b746ca0fcafff9
STHub65536 <2026opnewemp006@fullness.email>
2026-04-28 09:59:35 +0900

    finished Ex_03_14

---
+++ Ex_03_14/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex_03_14;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("値1を入力してください->");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("値2を入力してください->");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("加算: "+num1.Add(num2));
+        Console.WriteLine("平均: "+num1.Average(num2));
+    }
+}
