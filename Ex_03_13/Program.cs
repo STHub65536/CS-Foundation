af8907e81c91bbeec51fc099616fec1b4b998648
STHub65536 <2026opnewemp006@fullness.email>
2026-04-28 09:44:37 +0900

    finished Ex_03_13

---
+++ Ex_03_13/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex_03_13;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+        
+        Console.WriteLine("5 * 5 * "+Circle.PI+" = "+Circle.AreaCalculation(5));
+        Console.WriteLine("5 * 5 * "+Circle.PI+" = "+circle.AreaCalculation());
+
+        Circle.PI = 3.0;
+
+        Console.WriteLine("5 * 5 * "+Circle.PI+" = "+Circle.AreaCalculation(5));
+        Console.WriteLine("5 * 5 * "+Circle.PI+" = "+circle.AreaCalculation());
+    }
+}
