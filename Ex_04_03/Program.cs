a23aeb92bb01d87c2ee05903020f7e8cc1e62643
STHub65536 <2026opnewemp006@fullness.email>
2026-05-07 11:57:13 +0900

    fixed Ex_04_03

--- Ex_04_03/Program.cs
+++ Ex_04_03/Program.cs
@@ -7,20 +7,24 @@ static class Program
     static void Main(string[] args)
     {
         List<Employee> empList = new List<Employee>();
-        int id;
+        string id;
         string name;
         string address;
 
-        while(empList.Count < 2)
+        while(true)
         {
             Console.Write("社員番号を入力してください->");
-            id = int.Parse(Console.ReadLine());
+            id = Console.ReadLine();
+            if (id.Equals("End"))
+            {
+                break;
+            }
             Console.Write("社員名を入力してください->");
             name = Console.ReadLine();
             Console.Write("住所を入力してください->");
             address = Console.ReadLine();
 
-            empList.Add(new Employee(id, name, address));
+            empList.Add(new Employee(int.Parse(id), name, address));
         }
 
         for(int i = 0;i < empList.Count; i++)
